e79ebd92f9caa78911ebea81ad6d4c29f9f7fa6a
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-09-09 15:38:08 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Contacts from './components/contacts/Contacts';
+import Header from './components/layout/Header';
+import { Provider } from './context';
+import AddContact from './components/contacts/AddContact';
+class App extends Component {
+	render() {
+    return (
+			<Provider>
+				<div className='App'>
+					<Header branding='Contact Manager' />
+          <div className='container'>
+            <AddContact />
+						<Contacts />
+				  </div>
+				</div>
+			</Provider>
+		);
+	}
 }
-
 export default App;
---
+++ src/components/contacts/AddContact.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { Consumer } from '../../context'
+import uuid from  'uuid';
+
+class AddContact extends Component {
+	state = {
+		name: '',
+		email: '',
+		phone: ''
+	};
+
+	onSubmit = (dispatch, e) => {
+		e.preventDefault();
+		
+		const { name, email, phone } = this.state;
+
+		const newContact = {
+			id: uuid(),
+			name,
+			email,
+			phone
+		}
+		dispatch({ type: 'ADD_CONTACT', payload: newContact })
+		
+		//clear state
+		this.setState({
+			name: '',
+			email: '',
+			phone: ''
+		})
+	};
+	onChange = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
+	
+	render() {
+		const { name, email, phone } = this.state;
+
+		return (
+			<Consumer>
+				{value => {
+					const { dispatch } = value;
+					return (
+						<div className='card mb-3'>
+										<div className='card-header'>Add Contact</div>
+										<div className='card-body'>
+											<form onSubmit={this.onSubmit.bind(this, dispatch)}>
+												<div className='form-group'>
+													<label htmlFor='name'>Name</label>
+													<input
+														type='text'
+														name='name'
+														className='form-control form-control-lg'
+														placeholder='Enter Name...'
+														value={name}
+														onChange={this.onChange}
+													/>
+												</div>
+												<div className='form-group'>
+													<label htmlFor='email'>Email</label>
+													<input
+														type='email'
+														name='email'
+														className='form-control form-control-lg'
+														placeholder='Enter Email...'
+														value={email}
+														onChange={this.onChange}
+													/>
+												</div>
+												<div className='form-group'>
+													<label htmlFor='phone'>Phone</label>
+													<input
+														type='text'
+														name='phone'
+														className='form-control form-control-lg'
+														placeholder='Enter Phone...'
+														value={phone}
+														onChange={this.onChange}
+													/>
+												</div>
+												<input
+													type='submit'
+													value='Add Contact'
+													className='btn btn-light btn-block'
+												/>
+											</form>
+										</div>
+									</div>
+					)
+				}}
+			</Consumer>
+		)
+	}
+}
+
+export default AddContact;
